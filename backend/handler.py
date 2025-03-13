4248c49e4faa84990dfb91ae26ca1e823a9cce1a
matthew-wiblin <matthew@wibles.net>
2025-03-13 22:22:46 +0000

    lambda updated with cors

--- backend/handler.py
+++ backend/handler.py
@@ -6,6 +6,13 @@ def hello(event, context):
         "message": "Go Serverless v4.0! Your function executed successfully!",
     }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    response = {
+        "statusCode": 200, 
+        "headers": {
+            "Access-Control-Allow-Origin": "*",  # Allow all origins or specify 'http://localhost:5173'
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        "body": json.dumps(body)}
 
     return response
