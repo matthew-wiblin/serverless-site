4e34511c0c5ce23e70d5028e2c6adcebc8bce26d
matthew-wiblin <matthew@wibles.net>
2025-03-13 22:23:05 +0000

    lambda invoked on front end

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from "react";
+import { fetchHello } from "../lib/apiHandler";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchHello();
+      setMessage(data.message);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>home</div>
-  )
+    <div>
+      home
+      <p>{message}</p>
+    </div>
+  );
 }
