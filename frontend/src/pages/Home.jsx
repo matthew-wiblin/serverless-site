1dbe707c9f5642a9a4b28dc0a315a3b54005a3b9
matthew-wiblin <matthew@wibles.net>
2025-03-24 10:04:16 +0000

    dynamo setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,20 +3,27 @@ import { apiHandler } from "../lib/apiHandler";
 
 export default function Home() {
   const [message, setMessage] = useState("");
+  const [message2, setMessage2] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await apiHandler("users");
       setMessage(data.message);
     };
-
     fetchData();
+
+    const fetchData2 = async () => {
+      const data = await apiHandler("");
+      setMessage2(data.message);
+    };
+    fetchData2();
   }, []);
 
   return (
     <div>
       home
       <p>{message}</p>
+      <p>{message2}</p>
     </div>
   );
 }
