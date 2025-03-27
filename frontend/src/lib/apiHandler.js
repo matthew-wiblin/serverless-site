46f1fd45788ce7bd6d1b7c6675ba6b0230a3ead0
matthew-wiblin <matthew@wibles.net>
2025-03-27 22:59:56 +0000

    apihandler rewrite

--- frontend/src/lib/apiHandler.js
+++ frontend/src/lib/apiHandler.js
@@ -1,19 +1,27 @@
 const API = import.meta.env.VITE_API_URL;
 
 export async function apiHandler(path, method = "GET", body = null) {
-  const options = {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+  const url = `${API}${path}`;
+  const headers = { "Content-Type": "application/json" };
 
-  if (body !== null) {
-    options.body = JSON.stringify(body);
+  let options = { method, headers };
+
+  if (method === 'GET') {
+    console.log(`GET request to ${url}`)
+  } else if (['POST', 'PUT', 'PATCH'].includes(method)) {
+    if (body !== null) {
+      options.body = JSON.stringify(body);
+    }
+  } else {
+    console.log('method supplied not accounted for')
   }
 
-  const response = await fetch(`${API}${path}`, options);
-  const data = await response.json();
-  console.log(data);
-  return data;
+  try {
+    const response = await fetch(`${url}`, options);
+    const data = await response.json();
+    console.log('apiHandler data = ', data);
+    return data;
+  } catch (error) {
+    console.log('API HTTP error ', error)
+  }
 }
