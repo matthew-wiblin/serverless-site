eaa6cfacbbbbe4e71a3181c7cbc069854bcbaa3d
matthew-wiblin <matthew@wibles.net>
2025-04-08 22:24:56 +0100

    authentication function working

--- backend/lib/is_authenticated.py
+++ backend/lib/is_authenticated.py
@@ -1,6 +1,32 @@
 import json
 import requests
+import jwt
+from jwt.algorithms import RSAAlgorithm
 
 
-def is_authenticated():
-    pass
+def is_authenticated(token):
+    try:
+        accesstoken = token.split('Bearer ')[1]
+        decode = jwt.get_unverified_header(accesstoken)
+
+        jwks_url = 'https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_j2ARZDsZL/.well-known/jwks.json'
+        jwks = requests.get(jwks_url).json()
+
+        kid = decode['kid']
+        matchingkey = next(key for key in jwks['keys'] if key['kid'] == kid)
+
+        publickey = RSAAlgorithm.from_jwk(json.dumps(matchingkey))
+
+        decodedpayload = jwt.decode(
+            accesstoken,
+            publickey,
+            algorithms=['RS256']
+        )
+
+        if decodedpayload['token_use'] == 'access':
+            return decodedpayload['username']
+        else:
+            return 'error - Wrong token passed to backend'
+    except Exception as e:
+        return f"error - {str(e)}"
+
