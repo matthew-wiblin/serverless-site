100b25cb6bb2d1f25a20d8b2b59f9915d7e085d5
matthew-wiblin <matthew@wibles.net>
2025-07-15 22:07:27 +0100

    test folder setup

---
+++ backend/core/tests/debug.py
@@ -0,0 +1,9 @@
+from core.config.model import model
+from core.config.views import views
+from core.config.function import functions
+from core.handlers.apihandler import apihandler
+from core.handlers.viewhandler import viewhandler
+from core.lib.awsfunctions import getauthenticateduser, geturllist
+
+# For local testing of functions ...etc.
+
