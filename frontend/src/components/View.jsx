cca9612b1e5f08aab1ddc8b3a7b5f9ee241df68d
matthew-wiblin <matthew@wibles.net>
2025-07-24 17:22:54 +0100

    view component setup finalised

--- frontend/src/components/View.jsx
+++ frontend/src/components/View.jsx
@@ -1,41 +1,59 @@
-import { Title, Card, Text, Image } from '@mantine/core';
+import { Title, Card, Text, Image, Grid, Divider, Center } from '@mantine/core';
 
 export default function View({ viewData }) {
   const title = viewData?.title;
-  const clusters = viewData?.components;
+  const titlesize = viewData?.titlesize;
+  const border = viewData?.border
+  const components = viewData?.components;
 
   function renderTitle() {
-    return <Title order={1}>{title}</Title>;
+    return <Title order={titlesize}>{title}</Title>
   }
 
-  function renderComponent(component, key) {
-    if (component.type === 'box') {
-      return (
-        <Card key={key}>
-          <Title order={3}>{component.title}</Title>
-          {component.description.map((line, i) => (<Text key={i}>{line}</Text>))}
-        </Card>
-      );
-    }
-    if (component.type === 'photo') {
-      return (
-        <Image key={key} src={`${component.location}`} alt={component.location} w={500}/>
-      );
-    }
+  function parentDivProps() {
+    const props = {}
+    border ? props.style = { border: '1px solid #ccc', borderRadius: '20px', padding: '10px'} : null
+    return props
   }
 
-  function renderCluster(cluster, index) {
-    return (
-      <div key={index}>
-        {cluster.components.map((component, i) => renderComponent(component, i))}
-      </div>
-    );
+  function renderComponents() {
+    return Array.isArray(components) ? components.map((component, index) => renderComponent(component, index)) : null
+  }
+
+  function renderComponent(component, index) {
+    switch (component.type) {
+      case 'card':
+        return <Card key={index}>
+         <Title key={`card-title-${index}`} order={3}>{component.title}</Title>
+         {component.description.map((line, i) => (<Text key={`card-description-${index}-${i}`}>{line}</Text>))}
+       </Card>
+
+      case 'photo':
+        if (component.where === 'frontend') return <Image key={index} src={`${component.location}`} h='auto' w={500}/>
+
+      case 'divider':
+        return <Divider key={index}/>
+
+      case 'columns':
+        const span = 12 / component.components.length
+        return (<Grid key={index}> 
+          {component.components.map((colComponent, colIndex) => (
+            <Grid.Col key={`${index}-${colIndex}`} span={span}>
+              <Center style={{ flexDirection: 'column' }}>
+                {colComponent.map((subComponent, subIndex) => 
+                  renderComponent(subComponent, `${index}-${colIndex}-${subIndex}`)
+                )}
+              </Center>
+            </Grid.Col>
+          ))}
+        </Grid>);
+    }
   }
 
   return (
-    <div>
+    <div key={`view-${title}`} {...parentDivProps()}>
       {renderTitle()}
-      {clusters?.map((cluster, i) => renderCluster(cluster, i))}
+      {renderComponents()}
     </div>
   );
 }
