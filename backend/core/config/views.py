1f770a8e422ea777ec90d54d866132a8c5c42090
matthew-wiblin <matthew@wibles.net>
2025-07-24 17:23:10 +0100

    home view amended

--- backend/core/config/views.py
+++ backend/core/config/views.py
@@ -1,23 +1,26 @@
 views = {
     'home': {
         'title': 'Welcome to the Home Page',
+        'titlesize': 1,
         'border': True,
         'components': [
-            {'type': 'cluster', 'components': [
-                {'type': 'box', 'title': 'Overview of the site', 
-                    'description': ['This site is a simple photo uploader', 
-                        'You can sign up for an account and manage/store your photos']},
-                {'type': 'photo', 'where': 'frontend', 'location': 'spaniel-brown.png'}
+            {'type': 'divider'},
+            {'type': 'columns', 'components': [
+                [
+                    {'type': 'card', 'title': 'Overview of the site', 
+                        'description': ['This site is a simple photo uploader', 
+                            'You can sign up for an account and manage/store your photos']},
+                    {'type': 'photo', 'where': 'frontend', 'location': 'spaniel-brown.png'}
+                ],
+                [
+                    {'type': 'card', 'title': 'Architecture overview', 
+                        'description': ['Frontend is created in JS/React deployed to an S3 bucket w/ cloudfront and R53',
+                            'Authentication for logging in handled with AWS Cognito',
+                            'Backend in python deployed using the serverless framework to AWS lambda',
+                            'Data stored in a mixture of dynamodb and S3 buckets']},
+                    {'type': 'photo', 'where': 'frontend', 'location': 'diagram-infra.png'}
+                ]
             ]},
-            {'type', 'divider'},
-            {'type': 'cluster', 'components': [
-                {'type': 'box', 'title': 'Architecture overview', 
-                    'description': ['Frontend is created in JS/React deployed to an S3 bucket w/ cloudfront and R53',
-                        'Authentication for logging in handled with AWS Cognito',
-                        'Backend in python deployed using the serverless framework to AWS lambda',
-                        'Data stored in a mixture of dynamodb and S3 buckets']},
-                {'type': 'photo', 'where': 'frontend', 'location': 'diagram-infra.png'}
-            ]}
         ]
     },
     'profile': {
