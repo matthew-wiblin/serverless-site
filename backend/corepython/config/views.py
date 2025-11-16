8bcaaf44964732fd6ec229768fe74e86e828b138
matthew-wiblin <matthew@wibles.net>
2025-11-16 10:18:09 +0000

    view tidy up

--- backend/corepython/config/views.py
+++ backend/corepython/config/views.py
@@ -15,8 +15,8 @@ views = {
                 [
                     {'type': 'card', 'title': 'Architecture overview', 
                         'description': ['Frontend is created in JS/React deployed to an S3 bucket w/ cloudfront and R53',
-                            'Authentication for logging in handled with AWS Cognito',
-                            'Backend in python deployed using the serverless framework to AWS lambda',
+                            'Authentication is handled with AWS Cognito',
+                            'Backend in python and go deployed using the serverless framework to AWS lambda',
                             'Data stored in a mixture of dynamodb and S3 buckets']},
                     {'type': 'photo', 'where': 'frontend', 'location': 'diagram-infra.png'}
                 ]
