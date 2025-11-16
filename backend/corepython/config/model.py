332eaf8a67bd6806d23bef9b9d592c3627d50f35
matthew-wiblin <matthew@wibles.net>
2025-11-16 10:17:49 +0000

    config tidy up

--- backend/corepython/config/model.py
+++ backend/corepython/config/model.py
@@ -6,15 +6,11 @@ model = {
         ],
         'GSIs': []
     },
-    'photo': {
-        'primarykey': {'attributename': 'id', 'Label': 'User ID', 'type': 'integer'},
-        'attributes': [
-            {'attributename': 'username', 'Label': 'Username', 'type': 'string'},
-            {'attributename': 'photoname', 'Label': 'Photo Name', 'type': 'string'},
-            {'attributename': 'imageurl', 'Label': 'Image URL', 'type': 'string'},
-            {'attributename': 'createdat', 'Label': 'Created At', 'type': 'datetime'},
-            {'attributename': 'description', 'Label': 'Description', 'type': 'string'}
-        ],
-        'GSIs': []
-    }
+    # 'photo': {
+    #     'primarykey': {'attributename': 'id', 'Label': 'User ID', 'type': 'integer'},
+    #     'attributes': [
+    #         {'attributename': 'username', 'Label': 'Username', 'type': 'string'},
+    #     ],
+    #     'GSIs': []
+    # }
 }
