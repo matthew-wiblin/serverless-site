57a8da9e408f120524567e6a1ed0e6d1b78e7ab0
matthew-wiblin <matthew@wibles.net>
2026-01-03 18:00:59 +0000

    remaining home page clear up

--- backend/corepython/config/views.py
+++ backend/corepython/config/views.py
@@ -11,14 +11,13 @@ views = {
                         'description': ['This site is a simple file storage manager', 
                             'You can sign up for an account and manage/store your projects',
                             'Complete with audit and other functionality']},
-                    {'type': 'photo', 'where': 'frontend', 'location': 'spaniel-brown.png'}
+                    {'type': 'photo', 'where': 'frontend', 'location': 'spaniels-three.png'}
                 ],
                 [
                     {'type': 'card', 'title': 'Architecture overview', 
                         'description': ['Frontend is created in JS/React deployed to an S3 bucket w/ cloudfront and R53',
                             'Authentication is handled with AWS Cognito',
-                            'Backend in python and go deployed using the serverless framework to AWS lambda',
-                            'Data stored in a mixture of dynamodb and S3 buckets']},
+                            'Backend in python and go deployed using the serverless framework to AWS lambda']},
                     {'type': 'photo', 'where': 'frontend', 'location': 'diagram-infra.png'}
                 ]
             ]},
