264e2425ce93559a95a8c0cc6d95050c968a5901
matthew-wiblin <matthew@wibles.net>
2026-02-08 17:08:34 +0000

    debug update

--- backend/corepython/tests/debug.py
+++ backend/corepython/tests/debug.py
@@ -3,10 +3,9 @@ sys.dont_write_bytecode = True
 
 from corepython.config.model import model
 from corepython.config.views import views
-from corepython.config.buttons import buttons
 from corepython.handlers.apihandler import apihandler
 from corepython.handlers.viewhandler import viewhandler
-from corepython.lib.awsfunctions import getauthenticateduser, geturllist
 
 # For local testing of functions ...etc.
+
 
